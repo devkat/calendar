7c0bce62bd0cea9b9296cfc472c394575fbc4821
Andreas Hartmann <andreas.hartmann@becompany.ch>
2016-05-17 18:02:42 +0200

    Next steps.

--- client/src/components/calendar/calendar-week.js
+++ client/src/components/calendar/calendar-week.js
@@ -1,18 +1,21 @@
-System.register(["ramda"], function(exports_1, context_1) {
+System.register(["ramda", "moment"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var ramda_1;
+    var ramda_1, moment_1;
     var CalendarWeekCustomElement;
     return {
         setters:[
             function (ramda_1_1) {
                 ramda_1 = ramda_1_1;
+            },
+            function (moment_1_1) {
+                moment_1 = moment_1_1;
             }],
         execute: function() {
             class CalendarWeekCustomElement {
                 constructor() {
                     this.days = ramda_1.default.range(1, 8);
-                    this.hours = ramda_1.default.range(0, 24);
+                    this.hours = ramda_1.default.range(0, 24).map(h => moment_1.default().hour(h));
                 }
             }
             exports_1("CalendarWeekCustomElement", CalendarWeekCustomElement);
--- client/src/components/calendar/calendar-week.ts
+++ client/src/components/calendar/calendar-week.ts
@@ -1,9 +1,9 @@
 import R from "ramda";
+import moment from "moment";
 
 export class CalendarWeekCustomElement {
 
   private days:number[] = R.range(1, 8);
-  private hours:number[] = R.range(0, 24);
-
+  private hours:Object[] = R.range(0, 24).map(h => moment().hour(h));
 
 }
